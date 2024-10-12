3f9adbba1c89c1e9792ac9e8cdfa0d50122e73d4
l4nzel0d <borisych095@yandex.ru>
2024-10-12 21:45:11 +0300

    added simple counter functionality 9.3

--- practs/pract9/task9_3/task9_3.js
+++ practs/pract9/task9_3/task9_3.js
@@ -6,8 +6,22 @@ function toggleClass(element, className) {
     }
 }
 
+function updateCounter(element) {
+    const counter = element.querySelector('.counter');
+    let currentCount = parseInt(counter.textContent) || 0;
+
+    if (element.classList.contains('pressed')) {
+        counter.textContent = currentCount + 1;
+    } else {
+        counter.textContent = currentCount - 1;
+    }
+}
+
 const likeButtons = document.querySelectorAll('.like-button');
 
 likeButtons.forEach(likeButton => {
-    likeButton.addEventListener('click', () => toggleClass(likeButton, 'pressed'));
+    likeButton.addEventListener('click', () => {
+        toggleClass(likeButton, 'pressed');
+        updateCounter(likeButton);
+    });
 });
