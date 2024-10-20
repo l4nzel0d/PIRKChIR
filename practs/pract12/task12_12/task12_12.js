466010a1b4c6e82596313610235a49e2baf28952
l4nzel0d <borisych095@yandex.ru>
2024-10-20 19:10:22 +0300

    12_12 feat: add counter updating

--- practs/pract12/task12_12/task12_12.js
+++ practs/pract12/task12_12/task12_12.js
@@ -1,17 +1,27 @@
 const notificationContainer = document.querySelector(".notificationContainer");
+const notificationIconElement = document.querySelector(".notification-icon");
 const notificationsListContainer = document.querySelector(".notifications-list-container");
+const notificationCounterElement = document.querySelector(".notification-count");
 
-let notificationList = [];
+let notificationCount = 0;
 
 let timerNotificationCreator = setInterval(addNewNotification, 3000);
 
 function addNewNotification() {
     let notificationText = "New Notification";
-    notificationList.push(notificationText);
 
     const notificationElement = document.createElement('div');
     notificationElement.className = "notification-item";
     notificationElement.textContent = notificationText;
 
     notificationsListContainer.appendChild(notificationElement);
+    notificationCount++;
+    updateNotificationCount();
 }
+
+function updateNotificationCount() {
+    notificationCounterElement.textContent = notificationCount;
+}
+
+updateNotificationCount();
+
