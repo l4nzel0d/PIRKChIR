9848295b261a706257eb98fc3aa421864ebd537b
l4nzel0d <borisych095@yandex.ru>
2024-10-20 14:39:36 +0300

    12_4 done

---
+++ practs/pract12/task12_4/task12_4.js
@@ -0,0 +1,46 @@
+const textarea = document.querySelector('textarea');
+
+
+textarea.addEventListener('input', () => {
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`; 
+});
+
+textarea.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        if (event.shiftKey) {
+            return;
+        }
+        event.preventDefault();
+        createNotificationFromTextarea();
+    }
+})
+
+const button = document.getElementById('create-notification').addEventListener('click', createNotificationFromTextarea);
+
+
+function createNotificationFromTextarea() {
+    const notificationText = textarea.value;
+    if (notificationText.trim() === "") return;
+    showNotification(notificationText);
+    textarea.value = "";
+}
+
+function showNotification(options) {
+    const notification = document.createElement('div');
+    notification.className = 'notification';
+
+    const textInside = document.createElement('p');
+    textInside.innerHTML = options.trim().replace(/\n/g, '<br>');
+
+    notification.appendChild(textInside);
+    document.querySelector('.notifications-container').appendChild(notification);
+    
+    
+    setTimeout(() => {
+        notification.classList.add("fade-out");
+        setTimeout(() => {
+            notification.remove();
+        }, 300)
+    }, 1500);    
+}
