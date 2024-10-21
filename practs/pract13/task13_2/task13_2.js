82fc67f7762d0f51148e11c9408150b2f1003454
l4nzel0d <borisych095@yandex.ru>
2024-10-21 21:14:39 +0300

    13_2 feat: add button to remove notification, add animation on removing notification

--- practs/pract13/task13_2/task13_2.js
+++ practs/pract13/task13_2/task13_2.js
@@ -1,22 +1,22 @@
-const notificationContainer = document.querySelector(".notificationContainer");
-const notificationIconElement = document.querySelector(".notification-icon");
+const notificationContainer = document.querySelector(".notification-container");
 const notificationsListContainer = document.querySelector(".notifications-list-container");
 const notificationCounterElement = document.querySelector(".notification-count");
 
 let notificationCount = 0;
 
 let timerNotificationCreator;
+
 function startNotificationCreatorTimer() {
     timerNotificationCreator = setInterval(addNewNotification, 3000);
     console.log("Timer Set");
 } 
 
 function addNewNotification() {
-    let notificationText = "New Notification";
+    let notificationText = "New Notification!";
 
     const notificationElement = document.createElement('div');
     notificationElement.className = "notification-item";
-    notificationElement.textContent = notificationText;
+    notificationElement.innerHTML = `${notificationText} <i data-action="removeNotification" class="fa-regular fa-circle-xmark"></i></i>`;
 
     notificationsListContainer.appendChild(notificationElement);
     notificationCount++;
@@ -27,17 +27,31 @@ function updateNotificationCount() {
     notificationCounterElement.textContent = notificationCount;
 }
 
-
-notificationIconElement.addEventListener('click', () => {
-    clearInterval(timerNotificationCreator);
+notificationContainer.addEventListener('click', (event) => {
+    let bellIcon = event.target.closest('.bell-icon-container')
+    if (bellIcon) {
+        clearInterval(timerNotificationCreator);
     
-    console.log("Timer Cleared");
+        console.log("Timer Cleared");
+    
+        setTimeout(() => {
+            startNotificationCreatorTimer();
+        }, 10000);
+    }
 
-    setTimeout(() => {
-        startNotificationCreatorTimer();
-    }, 10000);
+    if (event.target.dataset.action == 'removeNotification') {
+        removeNotification(event.target.closest('.notification-item'));
+    }
 })
 
+function removeNotification(notification) {
+    notification.classList.add("in-removal");
+    setTimeout(() => {
+        notification.remove();
+        notificationCount--;
+        updateNotificationCount();
+    }, 400)
+}
 
 startNotificationCreatorTimer();
 updateNotificationCount();
@@ -46,11 +60,13 @@ const notificationStopStartButton = document.getElementById('notification-stop-s
 notificationStopStartButton.addEventListener('click', () => {
     if (notificationStopStartButton.dataset.action == "toStop") {
         clearInterval(timerNotificationCreator);
+        console.log("Timer Cleared");
         notificationStopStartButton.dataset.action = "toStart";
         notificationStopStartButton.textContent = "Start Notification Creation";
     }
     else {
         startNotificationCreatorTimer();
+        console.log("Timer Started");
         notificationStopStartButton.dataset.action = "toStop";
         notificationStopStartButton.textContent = "Stop Notification Creation";
     }
