0d57e65999e33207935b5ac4209e575b04251d6a
l4nzel0d <borisych095@yandex.ru>
2024-10-21 14:53:09 +0300

    13_1 fix: center both image and container using js

--- practs/pract13/task13_1/task13_1.js
+++ practs/pract13/task13_1/task13_1.js
@@ -1,31 +1,37 @@
-window.onload = function() {
-    console.log("onload");
-    const coordinatedDiv = document.getElementById('coordinates');
+const coordinatedDiv = document.getElementById('coordinates');
 
-    window.addEventListener('click', (event) => {
-        console.log("click");
-        const x = event.clientX;
-        const y = event.clientY;
-        coordinatedDiv.innerHTML = `Click coordinates: x = ${x}, y = ${y}`;
-    })
+window.addEventListener('click', (event) => {
+    console.log("click");
+    const x = event.clientX;
+    const y = event.clientY;
+    coordinatedDiv.innerHTML = `Click coordinates: x = ${x}, y = ${y}`;
+})
 
 
-    const image = document.getElementById('center-image');
+const image = document.getElementById('center-image');
+const imageContainer = document.getElementById('image-container');
 
-    function centerImage() {
-        const windowWidth = document.documentElement.clientWidth;
-        const windowHeight = document.documentElement.clientHeight;
-        const imageWidth = image.clientWidth;
-        const imageHeight = image.clientHeight;
+function centerElement(element) {
+    const windowWidth = document.documentElement.clientWidth;
+    const windowHeight = document.documentElement.clientHeight;
+    const elementWidth = element.clientWidth;
+    const elementHeight = element.clientHeight;
 
-        const left = (windowWidth - imageWidth) / 2;
-        const top = (windowHeight - imageHeight) / 2;
+    const left = (windowWidth - elementWidth) / 2;
+    const top = (windowHeight - elementHeight) / 2;
 
-        image.style.position = 'fixed';
-        image.style.left = `${left}px`;
-        image.style.top = `${top}px`;
-    }
+    element.style.position = 'fixed';
+    element.style.left = `${left}px`;
+    element.style.top = `${top}px`;
+}
 
-    centerImage();
-    window.addEventListener('resize', centerImage);
-};
+function centerContainerAndImage() {
+    centerElement(image);
+    centerElement(imageContainer);
+}
+
+window.addEventListener('resize', centerContainerAndImage);
+
+
+
+window.onload = centerContainerAndImage;
